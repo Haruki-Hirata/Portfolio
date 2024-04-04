6084195bd73b6f94158a3302735e46e13a848fbd
Haruki-Hirata <166073145+Haruki-Hirata@users.noreply.github.com>
2024-04-05 06:59:42 +0900

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,159 @@
+particlesJS.load('particles-js', 'assets/js/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+
+  // 動きのきっかけとなるアニメーションの名前を定義
+function fadeAnime(){
+
+  //ふわっと動くきっかけのクラス名と動きのクラス名の設定
+  $('.article-caption').each(function(){ //fadeInUpTriggerというクラス名が
+  　　var elemPos = $(this).offset().top-40; //要素より、50px上の
+  　　var scroll = $(window).scrollTop();
+  　　var windowHeight = $(window).height();
+  　　if (scroll >= elemPos - windowHeight){
+  　　$(this).addClass('fadeUp');
+  　　// 画面内に入ったらfadeInというクラス名を追記
+  　　}else{
+  　　　$(this).removeClass('fadeUp');
+  　　// 画面外に出たらfadeInというクラス名を外す
+  　　}
+  　　});
+  
+  //関数でまとめることでこの後に違う動きを追加することが出来ます
+  $('.fadeDownTrigger').each(function(){ //fadeInDownTriggerというクラス名が
+  　　var elemPos = $(this).offset().top-60; //要素より、50px上の
+  　　var scroll = $(window).scrollTop();
+  　　var windowHeight = $(window).height();
+  　　if (scroll >= elemPos - windowHeight){
+  　　$(this).addClass('fadeDown');
+  　　// 画面内に入ったらfadeDownというクラス名を追記
+  　　}else{
+  　　　$(this).removeClass('fadeDown');
+  　　// 画面外に出たらfadeDownというクラス名を外す
+  　　}
+  　　});
+  
+  
+  }//ここまでふわっと動くきっかけのクラス名と動きのクラス名の設定
+  
+  // 画面をスクロールをしたら動かしたい場合の記述
+    $(window).scroll(function (){
+      fadeAnime();/* アニメーション用の関数を呼ぶ*/
+    });// ここまで画面をスクロールをしたら動かしたい場合の記述
+  
+  // 画面が読み込まれたらすぐに動かしたい場合の記述
+    $(window).on('load', function(){
+      fadeAnime();/* アニメーション用の関数を呼ぶ*/
+    });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+
+
+
+
+//アコーディオンをクリックした時の動作
+$('.title').on('click', function() {//タイトル要素をクリックしたら
+	var findElm = $(this).next(".box");//直後のアコーディオンを行うエリアを取得し
+	$(findElm).slideToggle();//アコーディオンの上下動作
+    
+	if($(this).hasClass('close')){//タイトル要素にクラス名closeがあれば
+		$(this).removeClass('close');//クラス名を除去し
+	}else{//それ以外は
+		$(this).addClass('close');//クラス名closeを付与
+	}
+});
+
+//ページが読み込まれた際にopenクラスをつけ、openがついていたら開く動作※不必要なら下記全て削除
+$(window).on('load', function(){
+	$('.accordion-area li:first-of-type section').addClass("open"); //accordion-areaのはじめのliにあるsectionにopenクラスを追加
+	$(".open").each(function(index, element){	//openクラスを取得
+		var Title =$(element).children('.title');	//openクラスの子要素のtitleクラスを取得
+		$(Title).addClass('close');				//タイトルにクラス名closeを付与し
+		var Box =$(element).children('.box');	//openクラスの子要素boxクラスを取得
+		$(Box).slideDown(500);					//アコーディオンを開く
+	});
+});
+
+
+
+/*--------------------------------------------------------------------------*
+ *  
+ *  footerFixed.js
+ *  
+ *  MIT-style license. 
+ *  
+ *  2007 Kazuma Nishihata [to-R]
+ *  http://blog.webcreativepark.net
+ *  
+ *--------------------------------------------------------------------------*/
+
+new function(){
+	
+	var footerId = "footer";
+	//メイン
+	function footerFixed(){
+		//ドキュメントの高さ
+		var dh = document.getElementsByTagName("body")[0].clientHeight;
+		//フッターのtopからの位置
+		document.getElementById(footerId).style.top = "0px";
+		var ft = document.getElementById(footerId).offsetTop;
+		//フッターの高さ
+		var fh = document.getElementById(footerId).offsetHeight;
+		//ウィンドウの高さ
+		if (window.innerHeight){
+			var wh = window.innerHeight;
+		}else if(document.documentElement && document.documentElement.clientHeight != 0){
+			var wh = document.documentElement.clientHeight;
+		}
+		if(ft+fh<wh){
+			document.getElementById(footerId).style.position = "relative";
+			document.getElementById(footerId).style.top = (wh-fh-ft-1)+"px";
+		}
+	}
+	
+	//文字サイズ
+	function checkFontSize(func){
+	
+		//判定要素の追加	
+		var e = document.createElement("div");
+		var s = document.createTextNode("S");
+		e.appendChild(s);
+		e.style.visibility="hidden"
+		e.style.position="absolute"
+		e.style.top="0"
+		document.body.appendChild(e);
+		var defHeight = e.offsetHeight;
+		
+		//判定関数
+		function checkBoxSize(){
+			if(defHeight != e.offsetHeight){
+				func();
+				defHeight= e.offsetHeight;
+			}
+		}
+		setInterval(checkBoxSize,1000)
+	}
+	
+	//イベントリスナー
+	function addEvent(elm,listener,fn){
+		try{
+			elm.addEventListener(listener,fn,false);
+		}catch(e){
+			elm.attachEvent("on"+listener,fn);
+		}
+	}
+
+	addEvent(window,"load",footerFixed);
+	addEvent(window,"load",function(){
+		checkFontSize(footerFixed);
+	});
+	addEvent(window,"resize",footerFixed);
+	
+}
+
+
+
+
+
+
+
+  
